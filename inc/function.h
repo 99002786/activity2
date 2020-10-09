f07deeaa541a4417ab99ebd825f0c6ce021063d6
99002786 <72496123+99002786@users.noreply.github.com>
2020-10-09 17:22:12 +0530

    Create function.h

---
+++ inc/function.h
@@ -0,0 +1,19 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <stdio.h>
+int add(int operand1, int operand2);
+
+
+int subtract(int operand1, int operand2);
+
+
+int multiply(int operand1, int operand2);
+
+
+int divide(int operand1, int operand2);
+
+int factorial_func(int factorial_input);
+int prime_func(int prime_input);
+
+#endif
